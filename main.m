f726e878442ca474414055b305595f9ca26c01da
diegodelc <diegodelcastillo2001@gmail.com>
2023-03-11 15:19:34 +0000

    cowell's method works

--- main.m
+++ main.m
@@ -11,7 +11,7 @@ one_second = 1/(86400*365.25); %1 second in years
 acc_to_au_and_years = 6656.77641; %1m/s^2 to au/year^2
 
 
-%initial coordinates of spaceship (Sun-centered intertial frame)
+%initial coordinates of spaceship (Sun-centered inertial frame)
 r0 = [-1.05;0;0]; %au
 v0 = [0;-6.1316;0]; %au/year ^j
 
@@ -33,19 +33,18 @@ ad_vect =  @(r_mag,v_unit) aT0 * ((1./r_mag).^2 ).* (v_unit);
 
 tspan = [0,20];
 y0 = [r0;v0];
-[t,y] = ode45(@(t,y) cowell(y,mu,ad_vect),tspan,y0);
+[t_cowell,y_cowell] = ode45(@(t,y) cowell(y,mu,ad_vect),tspan,y0);
 
 
 
 val = 0;
 figure()
-plot(y(:,1+val),y(:,2+val),'-.')
+plot(0,0,'ro','DisplayName', 'SUN') %the sun
 hold on
-plot(0,0,'ro') %the sun
+plot(y_cowell(:,1+val),y_cowell(:,2+val),'-.b','DisplayName',"Cowell's Method")
+legend
 axis equal
-xlabel("i")
-ylabel("j")
-
+title("Cowell's method")
 
 %% Part 2 - Encke's Method
 
@@ -60,13 +59,13 @@ function stateSpaceRepCowell = cowell(y,mu,ad_fun)
     v = y(4:6);
     
     
-    v_mag = sum(v.^2);
+    v_mag = sqrt(sum(v.^2));
     v_unit = v./v_mag;
 
-    r_mag = sum(r.^2);
+    r_mag = sqrt(sum(r.^2));
 %     r_unit = r./r_mag;
     
-    ad = ad_fun(r_mag,v_unit);
+    ad = ad_fun(r_mag,v_unit)
 
     stateSpaceRepCowell = [v;ad-(mu.*r)./(r_mag.^3)];
 end
