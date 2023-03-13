e5bd3535bc0e6abad195ceacdf2bc34c7c1b9589
diegodelc <diegodelcastillo2001@gmail.com>
2023-03-13 11:41:40 +0000

    values are wrong but it runs

--- testing_encke.m
+++ testing_encke.m
@@ -3,7 +3,7 @@ close all
 
 
 %% Global
-
+global mu
 mu = 39.4769; %Sun's gravitational parameter, (au^3/year^2)
 
 %unit conversions
@@ -26,7 +26,7 @@ ad_vect =  @(r_mag,v_unit) aT0 * ((1./r_mag).^2 ).* (v_unit);
 
 
 %% Encke's Method
-J2 = 1082.63e-6;
+% J2 = 1082.63e-6;
 
 
 t = [0,20]; %in years
@@ -46,18 +46,20 @@ options = odeset('maxstep', del_t);
 %Begin Encke integration
 t = t0;
 tsave = t0;
-y = [R0 V0];
+y = [R0; V0]';
+%%
 del_y0 = zeros(6,1);
 
 t = t + del_t;
 
 while t <= tf + del_t/2
-    [dum,z] = ode45(@(t,f) rates(t,f,R0,V0,t0), [t0 t], del_y0, options);
+    [dum,z] = ode45(@(t,f) rates(t,f,R0,V0,t0,ad_vect), [t0 t], del_y0, options);
 
     [Rosc,Vosc] = rv_from_r0v0(R0, V0, t-t0);
     
-    R0 = Rosc + z(end,1:3);
-    V0 = Vosc + z(end,4:6);
+    R0 = Rosc' + z(end,1:3);
+    V0 = Vosc' + z(end,4:6);
+    
     t0 = t;
     
     tsave = [tsave;t];
@@ -65,8 +67,8 @@ while t <= tf + del_t/2
     t = t + del_t;
     del_y0 = zeros(6,1);
 end
-
-t = t_save;
+%%
+t = tsave;
 n_times = length(t);
 
 for j = 1:n_times
@@ -82,12 +84,75 @@ for j = 1:n_times
     w(j) = coe(5);
     TA(j) = coe(6);
 end
+
+%% Plotting orbit
+figure()
+plot(y(:,1),y(:,2))
+
+
 %%
 
 
 %Part 2: Encke's method (functions)
+function coe = coe_from_sv(R,V,mu)
+    eps = 1.e-10;
+    
+    r = norm(R);
+    v = norm(V);
 
-function dfdt = rates(t,f,R0,V0,t0)
+    vr = dot(R,V)/r;
+    H = cross(R,V);
+    h = norm(H);
+    
+    incl = acos(H(3)/h);
+    
+    N = cross([0 0 1],H);
+    n = norm(N);
+    
+    if n ~= 0
+        RA = acos(N(1)/n);
+        if N(2) < 0
+            RA = 2*pi - RA;
+        end
+    else
+    RA = 0;
+    end
+    
+    E = 1/mu*((v^2 - mu/r)*R - r*vr*V);
+    e = norm(E);
+    
+    if n ~= 0
+        if e > eps
+            w = acos(dot(N,E)/n/e);
+            if E(3) < 0
+                w = 2*pi - w;
+            end
+        else
+            w = 0;
+        end
+    else
+        w = 0;
+    end
+    %...Equation 4.13a (incorporating the case e = 0):
+    if e > eps
+        TA = acos(dot(E,R)/e/r);
+        if vr < 0
+            TA = 2*pi - TA;
+        end
+    else
+        cp = cross(N,R);
+        if cp(3) >= 0
+            TA = acos(dot(N,R)/n/r);
+        else
+            TA = 2*pi - acos(dot(N,R)/n/r);
+        end
+    end
+    
+    a = h^2/mu/(1 - e^2);
+    coe = [h e RA incl w TA a];
+end
+
+function dfdt = rates(t,f,R0,V0,t0,ad_vect)
     del_r = f(1:3)'; %Position deviation
     del_v = f(4:6)'; %Velocity deviation
     
@@ -96,22 +161,22 @@ function dfdt = rates(t,f,R0,V0,t0)
     
     Rpp = Rosc + del_r;
     Vpp = Vosc + del_v;
-    rosc = norm(Rosc);
-    rpp = norm(Rpp);
+%     rosc = norm(Rosc);
+%     rpp = norm(Rpp);
     
     %compute perturbing acceleration (here for J2, need to change to ours)
-    global J2;
-    global mu;
-    RE = earthRadius;
-    xx = Rpp(1); yy = Rpp(2); zz = Rpp(3);
-    fac = 3/2*J2*(mu/rpp^2)*(RE/rpp)^2;
-    ap = -fac*[(1 - 5*(zz/rpp)^2)*(xx/rpp) ...
-        (1 - 5*(zz/rpp)^2)*(yy/rpp) ...
-        (3 - 5*(zz/rpp)^2)*(zz/rpp)];
 
-    F = 1 - (rosc/rpp)^3;
-    del_a = -mu/rosc^3*(del_r - F*Rpp) + ap;
+%     RE = earthRadius;
+%     xx = Rpp(1); yy = Rpp(2); zz = Rpp(3);
+%     fac = 3/2*J2*(mu/rpp^2)*(RE/rpp)^2;
+%     ap = -fac*[(1 - 5*(zz/rpp)^2)*(xx/rpp) ...
+%         (1 - 5*(zz/rpp)^2)*(yy/rpp) ...
+%         (3 - 5*(zz/rpp)^2)*(zz/rpp)];
+% 
+%     F = 1 - (rosc/rpp)^3;
+%     del_a = -mu/rosc^3*(del_r - F*Rpp) + ap;
     
+    del_a = ad_vect(norm(Rpp),Vpp/norm(Vpp));
     %are these two the same?
     dfdt = [del_v(1) del_v(2) del_v(3) del_a(1) del_a(2) del_a(3)]';
 %     dfdt = [del_v del_a]';
@@ -133,7 +198,7 @@ function c = stumpC(z)
         c = (1 - cos(sqrt(z)))/z;
     elseif z < 0
         c = (cosh(sqrt(-z)) - 1)/(-z);
-        e44 MATLAB scripts
+        %%e44 MATLAB scripts
     else
         c = 1/2;
     end
@@ -153,15 +218,13 @@ function x = kepler_U(dt, ro, vro, a)
     while abs(ratio) > error && n <= nMax
         
         n= n+1;
-        C = stumpC(a*x^2)
-        S = stumpS(a*x^2)
-        ro
-        vro
-        x
+        C = stumpC(a*x^2);
+        S = stumpS(a*x^2);
+
         
-        F = ro*vro/sqrt(mu)*x^2*C + (1 - a*ro)*x^3*S + ro*x - sqrt(mu)*dt
-        dFdx = ro*vro/sqrt(mu)*x*(1 - a*x^2*S) + (1 - a*ro)*x^2*C + ro
-        ratio = F/dFdx
+        F = ro*vro/sqrt(mu)*x^2*C + (1 - a*ro)*x^3*S + ro*x - sqrt(mu)*dt;
+        dFdx = ro*vro/sqrt(mu)*x*(1 - a*x^2*S) + (1 - a*ro)*x^2*C + ro;
+        ratio = F/dFdx;
         x = x - ratio;
     end
     
@@ -200,7 +263,6 @@ function [R,V] = rv_from_r0v0(R0,V0,t)
     v0 = norm(V0);
     
     %initial radial velocity
-    R0
     vr0 = dot(R0,V0)/r0;
     
     %Reciprocal of the semimajor axis
